6410a77a1f7d79d5ad6e7fbc430b74d84f6e2e2a
kiranpe <kiranwls2011@gmail.com>
2020-06-21 16:03:32 +0530

    small changes

--- New-AllInOne/main.tf
+++ New-AllInOne/main.tf
@@ -1,15 +1,3 @@
-#module "k8s_instances" {
-#  source = "./k8s/modules/k8s"
-#}
-
-#module "jenkis_instance" {
-#   source = "./jenkins/modules/jenkins"
-#}
-
-#module "nexus" {
-#   source = "./nexus/modules/nexus"
-#}
-
-module "onefile" {
+module "allinone" {
   source = "./modules"
 }
